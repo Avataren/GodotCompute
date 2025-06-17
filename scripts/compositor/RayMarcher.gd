68672115ba132df8d1ed4ed4fea7bbfa045d8065
Morten Andersson <morten.andersson@gmail.com>
2025-06-17 17:04:09 +0200

    working depth sampler

--- scripts/compositor/RayMarcher.gd
+++ scripts/compositor/RayMarcher.gd
@@ -46,35 +46,52 @@ func _render_callback(_effect_callback_type: int, render_data: RenderData) -> vo
 	var y_groups : int = ceili(size.y / float(LOCAL_WORKGROUP_Y))
 	
 	var cam_xform : Transform3D = scene_data.get_cam_transform()
+	var inv_proj_mat : Projection = scene_data.get_cam_projection().inverse()
 	
 	for view in scene_buffers.get_view_count():
 		var proj : Projection = scene_data.get_view_projection(view)
 		var fov_deg : float  = proj.get_fov()
-		var push_constants := _fill_push_constants(cam_xform, fov_deg, size)
+		var push_constants := _fill_push_constants(cam_xform, fov_deg, size, inv_proj_mat[2].w, inv_proj_mat[3].w)
 		var screen_tex:RID = scene_buffers.get_color_layer(view)
+		var depth_tex:RID = scene_buffers.get_depth_layer(view)
+		
 		var uniform :RDUniform = RDUniform.new()
 		uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
 		uniform.binding = 0
 		uniform.add_id(screen_tex)
 		
 		var image_uniform_set : RID
-		if (Engine.is_editor_hint()):
-			image_uniform_set = rd.uniform_set_create([uniform], shader, 0)
-		else:
-			image_uniform_set = UniformSetCacheRD.get_cache(shader, 0, [uniform])
+		#if (Engine.is_editor_hint()):
+			#image_uniform_set = rd.uniform_set_create([uniform], shader, 0)
+		#else:
+		image_uniform_set = UniformSetCacheRD.get_cache(shader, 0, [uniform])
+		
+		var sampler_state : RDSamplerState = RDSamplerState.new()
+		sampler_state.min_filter = RenderingDevice.SAMPLER_FILTER_LINEAR
+		sampler_state.mag_filter = RenderingDevice.SAMPLER_FILTER_LINEAR
+		var sampler : RID = rd.sampler_create(sampler_state)
+		
+		uniform = RDUniform.new()
+		uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_SAMPLER_WITH_TEXTURE
+		uniform.binding = 0
+		uniform.add_id(sampler)
+		uniform.add_id(depth_tex)
+		
+		var depth_uniform_set:RID = UniformSetCacheRD.get_cache(shader, 1, [uniform])
 		
 		var compute_list :int = rd.compute_list_begin()
 		rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
 		rd.compute_list_bind_uniform_set(compute_list, image_uniform_set, 0)
+		rd.compute_list_bind_uniform_set(compute_list, depth_uniform_set, 1)
 		rd.compute_list_set_push_constant(compute_list, push_constants, push_constants.size())
 		rd.compute_list_dispatch(compute_list, x_groups, y_groups,1)
 		rd.compute_list_end()
 	
-func _fill_push_constants(cam_xform: Transform3D, fov: float,  screen_size: Vector2i) -> PackedByteArray:
+func _fill_push_constants(cam_xform: Transform3D, fov: float,  screen_size: Vector2i, inv_proj2w: float, inv_proj3w: float) -> PackedByteArray:
 	# Get the camera's full transform.
 
 	var floats = PackedFloat32Array()
-	floats.resize(20)
+	floats.resize(24)
 	# Column 0 (Basis X vector)
 	floats[0] = cam_xform.basis.x.x
 	floats[1] = cam_xform.basis.x.y
@@ -100,6 +117,11 @@ func _fill_push_constants(cam_xform: Transform3D, fov: float,  screen_size: Vect
 	floats[17] = screen_size.y
 	# --- 3. Params.fov_rad (float) - 4 bytes (Float 18) ---
 	floats[18] = deg_to_rad(fov)
-	floats[19] = 0.0 #padding
+	floats[19] = inv_proj2w
+	floats[20] = inv_proj3w
+	floats[21] = 0.0
+	floats[22] = 0.0
+	floats[23] = 0.0
+	
 
 	return floats.to_byte_array()
--- shaders/compositor/RayMarcher.glsl
+++ shaders/compositor/RayMarcher.glsl
@@ -4,21 +4,28 @@
 layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
 layout(rgba16f, binding = 0, set = 0) uniform image2D screen_tex;
+layout(binding = 0, set = 1) uniform sampler2D depth_tex;
 
 layout(push_constant, std430) uniform Params {
     mat4 camera_transform;
     vec2 screen_size;
     float fov_rad;
+    float inv_proj_2w;
+    float inv_proj_3w;
 } params;
-
 
 void main() {
     ivec2 pixel = ivec2(gl_GlobalInvocationID.xy);
     vec2 size = params.screen_size;
+    vec2 uv = pixel / size;
 
     if (pixel.x >= size.x || pixel.y >= size.y) return;
 
+    float depth = texture(depth_tex, uv).r;
+    float linear_depth = 1. / (depth * params.inv_proj_2w + params.inv_proj_3w);
+    linear_depth = clamp(linear_depth / 50, 0.0, 1.0);
+
     vec4 color = imageLoad(screen_tex, pixel);
-    color.rgb *= vec3(1.0, 1.0, 0.5);
+    color.rgb = vec3(linear_depth);
     imageStore(screen_tex, pixel, color);
 }
